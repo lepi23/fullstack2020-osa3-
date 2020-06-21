96ddde6803dfb2d8dc65ca650943bf976f4501bc
Leevi Saarinen <leevi.saarinen@tuni.fi>
2020-06-21 14:28:02 +0300

    frontendind buildi hakemistoon

---
+++ build/precache-manifest.35c9e3e7a9b3b76e72bbbd19a14435f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11e7db5671dbea3f66e2cfeec18cff2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8934baa415c7b5bb5dda",
+    "url": "/static/css/main.f0a3268e.chunk.css"
+  },
+  {
+    "revision": "5261b682071615555f7e",
+    "url": "/static/js/2.bcb43e66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bcb43e66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8934baa415c7b5bb5dda",
+    "url": "/static/js/main.cff2e6a5.chunk.js"
+  },
+  {
+    "revision": "24aecb121d5a1e31ca85",
+    "url": "/static/js/runtime-main.455ce434.js"
+  }
+]);
