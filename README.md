1df1c3e2eb9ec75834d3ca8d73b44bf8a22179a9
Leevi Saarinen <leevi.saarinen@tuni.fi>
2020-06-23 18:18:40 +0300

    mongoDB:n lisäys

---
+++ README.md
@@ -0,0 +1,3 @@
+http://secret-mesa-93611.herokuapp.com/
+
+mongodb+srv://fullstack:<password>@cluster0-vplhm.mongodb.net/<dbname>?retryWrites=true&w=majority
--- index.js
+++ index.js
@@ -1,51 +1,36 @@
 
+require('dotenv').config()
 const express = require('express')
 const app = express()
+const Person = require('./models/person')
 
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
 
 var morgan = require('morgan')
-//morgan.token('postData', (req, res) => { return req.body[''] })
 morgan.token('type', function (req, res) { return JSON.stringify(req.body)})
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :type'))
 
-
-var persons =
-[
-    { 
-      name: "Arto Hellas", 
-      number: "040-123456",
-      id: 1
-    },
-    { 
-      name: "Ada Lovelace", 
-      number: "39-44-5323523",
-      id: 2
-    },
-    { 
-      name: "Dan Abramov", 
-      number: "12-43-234345",
-      id: 3
-    },
-    { 
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122",
-      id: 4
-    }
-]
+//info sivu
 app.get('/info', (req, res) => {
   res.send(
-    `<p>PhoneBook has info for ${persons.length} people</p>
+    `<p>PhoneBook has info for ${Person.countDocuments({})} people</p>
     <p>${new Date()}</p>`)
 })
-
+//kaikkien henkilöiden datan hakeminen tietokannasta
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
+})
+/*
 app.get('/api/persons', (req, res) => {
 res.json(persons)
 })
-
+*/
+/*
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
@@ -58,15 +43,39 @@ app.get('/api/persons/:id', (req, res) => {
     res.status(404).end()
   }
   
-}) 
+})*/
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+  .then(person => {
+    if(person){
+      res.json(person)
+    }
+    else{
+      res.status(404).end()
+    }
+  })
+  .catch(error => next(error))
+})
+//yksittäisen henkilön poisto listalta
+/*
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
 
   res.status(204).end()
+})*/
+app.delete('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+//henkilön lisäys
 app.post('/api/persons', (req, res) => {
   const body = req.body
+
   if (!body.name) {
     return res.status(400).json({ 
       error: 'name missing' 
@@ -77,16 +86,37 @@ app.post('/api/persons', (req, res) => {
       error: 'number missing' 
     })
   }
-  const person = {
+  const person = new Person({
     name : body.name,
     number : body.number,
-    id : Math.floor(Math.random() * Math.floor(5000))
-  }
-  persons = persons.concat(person)
-  res.json(person)
+    id : Math.floor(Math.random() * Math.floor(5000))  
+  })
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 })
+//henkilön numeron muutos
+app.put('/api/persons/:id', (req, res, next) => {
+  Person.findByIdAndUpdate(req.params.id,{name : req.body.name, number: req.body.number})
+    .then(result => {
+      res.status(204).end()
+    })
+    .catch(error => next(error))
+})
+//virheellisten pyyntöjen käsittely
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
 
-const PORT = process.env.PORT || 3001
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
